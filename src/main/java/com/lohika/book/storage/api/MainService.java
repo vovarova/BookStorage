6b97431210fb173a105dfb30dba06c060389b23f
vovarova <vova.rova@gmail.com>
2013-11-06 13:27:48 +0200

    Add new files

--- src/main/java/com/lohika/book/storage/api/MainService.java
+++ src/main/java/com/lohika/book/storage/api/MainService.java
@@ -1,8 +1,17 @@
 package com.lohika.book.storage.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 /**
  * @author vroman
- * Test service
+ *         Test service
  */
+@Path("/")
 public class MainService {
+    @GET
+    public String getIndexPage() {
+        return "Hello World";
+    }
+
 }
