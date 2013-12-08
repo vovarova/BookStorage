3d1ea85f26512b4d3521a0f30b45c5634e664580
vovarova <vova.rova@gmail.com>
2013-12-08 17:57:54 +0200

    Add Stubs for file service

--- src/main/java/com/lohika/book/storage/service/implementation/FileServiceImpl.java
+++ src/main/java/com/lohika/book/storage/service/implementation/FileServiceImpl.java
@@ -43,7 +43,7 @@ public class FileServiceImpl implements FileService {
                 LOGGER.error(e);
             }
         }
-        return fileStoragePath;
+        return "";
     }
 
     /**
